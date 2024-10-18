e8ada3f47133571c97315084dc64e771dc67ecc5
Andrew <andrew155273@gmail.com>
2024-10-18 16:51:50 +0800

    1018_1651

--- lesson4/lesson4_6.py
+++ lesson4/lesson4_6.py
@@ -13,7 +13,7 @@ class Window(ThemedTk):
         btn2.pack(side='left',expand=True,fill='x')
         btn3 = ttk.Button(topFrame,text="按鈕3")
         btn3.pack(side='left',expand=True,fill='x',padx=10)
-        topFrame.pack(padx=10,pady=(10,0),ipadx=10,ipady=10,expand=True,fill='x')
+        topFrame.pack(padx=10,pady=(10,0),ipadx=10,ipady=10,expand=True,fill='x')      
         bottomFrame = ttk.Frame(self,width=500,height=300,borderwidth=1,relief='groove')
         bottomFrame.pack(padx=10,pady=10)
 
