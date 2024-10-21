bf336d424e321732adf1b3e0970a99eb981e94e6
Andrew <andrew155273@gmail.com>
2024-10-21 09:10:08 +0800

    1021_0910

--- lesson4/lesson4_homework.py
+++ lesson4/lesson4_homework.py
@@ -7,6 +7,7 @@ class Window(ThemedTk):
         self.title('呂安杰的lesson4作業')
         style = ttk.Style(self)        
 
+#=============================== TopFrame =====================================
         topFrame = ttk.Frame(self,borderwidth=1,relief='solid')
         btn1 = ttk.Button(topFrame,text="。")
         btn1.pack(side='left',expand=True,fill='x',padx=(10,30))
@@ -15,7 +16,9 @@ class Window(ThemedTk):
         btn3 = ttk.Button(topFrame,text="。")
         btn3.pack(side='left',expand=True,fill='x',padx=(30,10))
         topFrame.pack(padx=10,pady=10,ipadx=10,ipady=10,expand=True,fill='x')     
+#==============================End TopFrame ===================================
 
+#============================= BottomFrame 1 ==================================
         bottomFrame = ttk.Frame(self,borderwidth=1,relief='solid')
         btn1 = ttk.Button(bottomFrame,text="。")
         btn1.pack(side='top',expand=True,fill='x',padx=10,ipady=30,pady=(5,0))
@@ -24,7 +27,8 @@ class Window(ThemedTk):
         btn3 = ttk.Button(bottomFrame,text="。")
         btn3.pack(side='top',expand=True,fill='x',padx=10,ipady=15,pady=(0,5))
         bottomFrame.pack(side='left',padx=10,pady=(0,10),ipadx=10,ipady=15,expand=True,fill='x')
-
+#==========================End BottomFrame 1 ==================================
+#============================= BottomFrame 2 ==================================
         bottomFrame = ttk.Frame(self,borderwidth=1,relief='solid')
         btn1 = ttk.Button(bottomFrame,text="。")
         btn1.pack(side='top',expand=True,fill='x',padx=10,ipady=25,pady=(5,0))
@@ -33,7 +37,8 @@ class Window(ThemedTk):
         btn3 = ttk.Button(bottomFrame,text="。")
         btn3.pack(side='top',expand=True,fill='x',padx=10,ipady=25,pady=(0,5))
         bottomFrame.pack(side='left',pady=(0,10),ipadx=10,ipady=15,expand=True,fill='x')
-
+#==========================End BottomFrame 2 ==================================
+#============================= BottomFrame 3 ==================================
         bottomFrame = ttk.Frame(self,borderwidth=1,relief='solid')
         btn1 = ttk.Button(bottomFrame,text="。")
         btn1.pack(side='top',expand=True,fill='x',padx=10,ipady=20,pady=(5,0))
@@ -42,7 +47,7 @@ class Window(ThemedTk):
         btn3 = ttk.Button(bottomFrame,text="。")
         btn3.pack(side='top',expand=True,fill='x',padx=10,ipady=20,pady=(0,5))
         bottomFrame.pack(side='left',padx=10,pady=(0,10),ipadx=10,ipady=15,expand=True,fill='x')
-
+#==========================End BottomFrame 3 ==================================
 def main():
     window = Window(theme='awlight')
     window.mainloop()
