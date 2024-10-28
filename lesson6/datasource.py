9a236f9b41073559786ee8c386b5d1249b49ddbf
Andrew <andrew155273@gmail.com>
2024-10-28 16:49:41 +0800

    1028_1649

--- lesson6/datasource.py
+++ lesson6/datasource.py
@@ -19,3 +19,22 @@ def get_sitename()->list[str]:
 
 # a=get_sitename()
 # print(a)
+
+
+def get_selected_data(sitename:str)->list[str]:
+    url='https://data.moenv.gov.tw/api/v2/aqx_p_432?api_key=e8dd42e6-9b8b-43f8-991e-b3dee723a52d&limit=1000&sort=ImportDate%20desc&format=JSON'
+    try:
+        respone=requests.get(url)
+        respone.raise_for_status()
+        data=respone.json()
+        type(respone)
+    except Exception as e:
+        print(e)
+    else:
+        outerlist=[sitename]
+        for items in (data['records']):
+            if items['sitename']==sitename:
+                innerlist=[items['publishtime'],items['county'],items['aqi'],items['pm2.5'],items ['status'],items['latitude'],items['longitude']]
+                outerlist.append(innerlist)
+            
+    return outerlist
--- lesson6/lesson6_2.py
+++ lesson6/lesson6_2.py
@@ -17,30 +17,64 @@ class Window(ThemedTk):
         #==============top Frame===============
 
         topFrame = ttk.Frame(self)
-        ttk.Label(topFrame,text='空氣品質',style='TopFrame.TLabel').pack()
+        ttk.Label(topFrame,text='空氣品質指標(AQI)(歷史資料)',style='TopFrame.TLabel').pack()
         topFrame.pack(padx=20,pady=20)
         
         #==============end topFrame===============
 
         #==============bottomFrame===============
         bottomFrame = ttk.Frame(self)
-        sitename=datasource.get_sitename()
+        sitenames = datasource.get_sitename()
+        self.selected_site = tk.StringVar()
+        sitenames_cb = ttk.Combobox(bottomFrame, textvariable=self.selected_site,values=sitenames,state='readonly')
+        self.selected_site.set('請選擇站點')
+        sitenames_cb.bind('<<ComboboxSelected>>', self.sitename_selected)
+        sitenames_cb.pack(side='left',expand=True,anchor='n')        
         
-        self.select_sitename=tk.StringVar()
-        sitename_cb=ttk.Combobox(bottomFrame,textvariable=self.select_sitename,values=sitename,state='readonly')
-        self.select_sitename.set(0,'請選擇站點')
-        sitename_cb.pack()
+        
 
+        # define columns
+        columns = ('date', 'county', 'aqi', 'pm25','status','lat','lon')
+
+        self.tree = ttk.Treeview(bottomFrame, columns=columns, show='headings')
+
+        # define headings
+        self.tree.heading('date', text='日期')
+        self.tree.heading('county', text='縣市')
+        self.tree.heading('aqi', text='AQI')
+        self.tree.heading('pm25', text='PM2.5')
+        self.tree.heading('status',text='狀態')
+        self.tree.heading('lat', text='緯度')
+        self.tree.heading('lon', text='經度')
+
+        self.tree.column('date', width=150,anchor="center")
+        self.tree.column('county', width=80,anchor="center")
+        self.tree.column('aqi', width=50,anchor="center")
+        self.tree.column('pm25', width=50,anchor="center")
+        self.tree.column('status', width=50,anchor="center")
+        self.tree.column('lat', width=100,anchor="center")
+        self.tree.column('lon', width=100,anchor="center")
+
+        # generate sample data
+        #contacts = []
+        #for n in range(1, 100):
+        #    contacts.append((f'first {n}', f'last {n}', f'email{n}@example.com'))
+
+        # add data to the treeview
+        #for contact in contacts:
+        #    tree.insert('', tk.END, values=contact)
         
+        self.tree.pack(side='right')
         bottomFrame.pack(expand=True,fill='x',padx=20,pady=(0,20),ipadx=10,ipady=10)
-        #==============end bottomFrame===============
 
-    def agreement_changed(self):
-        showinfo(
-            title='Agreement',
-            message= self.agreement.get()
-
-        )
+            #==============end bottomFrame===============
+        
+    def sitename_selected(self,event):
+        selected = self.selected_site.get()
+        print(selected)
+        # selected_data = datasource.get_selected_data(selected)
+        # for record in selected_data:
+        #     self.tree.insert("", "end", values=record)
     
         
 
